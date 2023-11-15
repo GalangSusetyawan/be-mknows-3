17207e67b7577ca35bec740f48451594367accfd
Galang <galangs123@gmail.com>
2023-11-15 19:50:45 +0700

    init

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { Routes } from "@interfaces/routes.interface";
+
+import { AuthController } from "@controllers/auth.controller";
+
+import { AuthMiddleware } from "@middlewares/auth.middleware";
+import RateLimiter from "@/middlewares/rate-limitter.middleware";
+import { ValidationMiddleware } from "@middlewares/validation.middleware";
+
+import { CreateUserDto, LoginUserDto } from "@dtos/users.dto";
+
+export class AuthRoute implements Routes {
+  public path = "auth";
+  public router = Router();
+  public auth = new AuthController();
+
+  public rateLimiter = new RateLimiter();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(`/v1/${this.path}/register`, ValidationMiddleware(CreateUserDto), this.auth.signUp);
+    this.router.post(`/v1/${this.path}/login`, ValidationMiddleware(LoginUserDto), this.auth.logIn);
+    this.router.post(`/v1/${this.path}/logout`, AuthMiddleware, this.auth.logOut);
+
+    this.router.post(`/v1/${this.path}/verify`, this.auth.verifyEmail);
+    this.router.post(`/v1/${this.path}/email/resend`,
+      this.rateLimiter.emailVerification,
+      this.auth.verifyEmail);
+  }
+}
